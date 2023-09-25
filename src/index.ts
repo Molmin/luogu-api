e8cfebd626cd1f1c179b64b3cc4efdfb9eb2fab5
Milmon <107737161+Molmin@users.noreply.github.com>
2023-09-25 19:26:51 +0800

    auto login

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { BasicService, UserAgent } from './lib'
+import { writeFileSync } from 'fs'
+import superagent from 'superagent'
 import { JSDOM } from 'jsdom'
 
 export class LuoguAccountService extends BasicService {
@@ -13,23 +15,29 @@ export class LuoguAccountService extends BasicService {
         };
     }
 
+    async reset() {
+        this.cookie = [];
+        delete this.postHeader['X-Csrf-Token'];
+    }
+
     async getCookie() {
         const response = await this.get('/');
         this.cookie = response.headers['set-cookie'];
         this.log(this.cookie);
     }
 
-    async getLoginCaptcha() {
+    async getLoginCaptcha(autosave?: string) {
         await this.getCookie();
         const response = await this.get('/user/setting');
         const { window: { document } } = new JSDOM(response.text);
         this.postHeader['X-Csrf-Token'] = document
             .querySelector('meta[name="csrf-token"]')?.getAttribute('content');
-        const captcha = await this.get(`/api/verify/captcha`)
+        const { body: captcha } = await this.get(`/api/verify/captcha`)
             .query({ _t: new Date().getTime() });
-        return captcha.body;
+        if (autosave) writeFileSync(autosave, captcha, "binary");
+        return captcha;
     }
-    login(username: string, password: string, captcha: string) {
+    login(username: string, password: string, captcha: string): Promise<Boolean> {
         return new Promise((resolve) =>
             this.post('/api/auth/userPassLogin')
                 .set('referer', 'https://www.luogu.com.cn/user/setting')
@@ -46,5 +54,19 @@ export class LuoguAccountService extends BasicService {
                     }
                 })
         );
+    }
+    async autoLogin(username: string, password: string, retry: number = 2): Promise<Boolean> {
+        this.reset();
+        const CaptchaFile = await this.getLoginCaptcha();
+        const DataURL: string = 'data:image/jpeg;base64,' + CaptchaFile.toString('base64');
+        const { text: captcha } = await superagent
+            .post('https://luogu-captcha-bypass.piterator.com/predict/')
+            .set('Content-Type', 'application/json')
+            .send(DataURL);
+        if (retry > 0) {
+            if (await this.login(username, password, captcha)) return true;
+            else return await this.autoLogin(username, password, retry - 1);
+        }
+        else return await this.login(username, password, captcha);
     }
 }
